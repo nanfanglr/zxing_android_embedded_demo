764375f1ebc7528dc88937da3fa52414fceefacc
nanfanglr <nanfanglr@sina.com>
2018-07-23 10:06:25 +0800

    添加readme文档及布局的修改

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# zxing_android_embedded_demo
-根据ZXing Android Embedded 库来实现项目
+**# zxing_android_embedded_demo**
+
+_根据[ZXing Android Embedded](https://github.com/journeyapps/zxing-android-embedded)库来实现项目_
+
+1、自定义scannerview的样式:
+参考[issues337](https://github.com/journeyapps/zxing-android-embedded/issues/337)
+
+2、了解条形码、二维码的格式
--- app/src/main/java/com/rui/zxing_android_embedded_demo/MainActivity.java
+++ app/src/main/java/com/rui/zxing_android_embedded_demo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.rui.zxing_android_embedded_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
