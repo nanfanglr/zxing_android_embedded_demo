5991f50ad68f262266edc28175f8aca0012d2ffc
nanfanglr <nanfanglr@sina.com>
2018-07-24 14:34:04 +0800

    添加了barcodescanner库的示例

--- barcodescannerapplication/src/main/java/com/rui/barcodescannerapplication/MainActivity.java
+++ barcodescannerapplication/src/main/java/com/rui/barcodescannerapplication/MainActivity.java
@@ -1,13 +1,47 @@
 package com.rui.barcodescannerapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ZXING_CAMERA_PERMISSION = 1;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.button2)
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, ZXING_CAMERA_PERMISSION);
+        }
+    }
+
+    @OnClick({R.id.button, R.id.button2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                startActivity(new Intent(this, ScannerActivity.class));
+                break;
+            case R.id.button2:
+                break;
+        }
     }
 }
--- barcodescannerapplication/src/main/java/com/rui/barcodescannerapplication/ScannerActivity.java
+++ barcodescannerapplication/src/main/java/com/rui/barcodescannerapplication/ScannerActivity.java
@@ -1,13 +1,64 @@
 package com.rui.barcodescannerapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ScannerActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    @BindView(R.id.tv_header)
+    TextView tvHeader;
+    @BindView(R.id.zxingScannerView)
+    ZXingScannerView zxingScannerView;
+    @BindView(R.id.tv_footer)
+    TextView tvFooter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        ButterKnife.bind(this);
+        zxingScannerView.setAutoFocus(true);
+        zxingScannerView.setAspectTolerance(0.5f);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        zxingScannerView.stopCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        zxingScannerView.setResultHandler(this);
+        zxingScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(this, "Contents = " + rawResult.getText() +
+                ", Format = " + rawResult.getBarcodeFormat().toString(), Toast.LENGTH_SHORT).show();
+
+        // Note:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                zxingScannerView.resumeCameraPreview(ScannerActivity.this);
+            }
+        }, 2000);
+    }
+
 }
